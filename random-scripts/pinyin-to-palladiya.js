cc7ed110bd0bf77258fc9bdd062701d97bf90c6c
Serhii Khoma <srghma@gmail.com>
2021-02-17 17:31:16 +0200

    feat: update

--- random-scripts/pinyin-to-palladiya.js
+++ random-scripts/pinyin-to-palladiya.js
@@ -1,43 +1,67 @@
+// node --experimental-repl-await
+
 const csv = require('csv-parser')
 const fs = require('fs')
-const pinyinConvert = require('pinyin-convert')
+// const pinyinConvert = require('pinyin-convert')
+const R = require('ramda')
 
-let results = [];
-let headers = [ "sentence", "pinyin" ]
-fs.createReadStream('/home/srghma/Downloads/Chinese Grammar Wiki.txt').pipe(csv({ separator: '\t', headers })).on('data', (data) => results.push(data)).on('end', () => { console.log(results); })
+function readStreamArray(stream) {
+  return new Promise((resolve, reject) => {
+      const data = []
 
-let res = results.map(x => ({ sentence: x['sentence'], pinyin: pinyinConvert(x['pinyin']) }))
+      stream.on("data", chunk => data.push(chunk))
+      stream.on("end", () => resolve(data))
+      stream.on("error", error => reject(error))
+  })
+}
 
-let res2 = null
+input = await readStreamArray(fs.createReadStream('/home/srghma/Downloads/Chinese Grammar Wiki.txt').pipe(csv({ separator: "\t", headers: [ "sentence", "pinyin_marked" ] })))
 
-Promise.all(
-  res
-  .map(x =>
-    x['pinyin']
-    .then(p => ({ sentence: x['sentence'], p })
-      )
-    )
-  )
-  .then(x => { res2 = x })
+convertToRuTable = await readStreamArray(fs.createReadStream('/home/srghma/projects/anki-cards-from-pdf/pinyin-to-ru-by-kfcd').pipe(csv({ separator: '\t', headers: ["from", "2", '3', '4', '5', '6', '7', '8', '9', 'to', '11', '12'] })))
 
-let convertToRuTable = [];
-fs.createReadStream('/home/srghma/projects/anki-cards-from-pdf/pinyin-to-ru-by-kfcd').pipe(csv({ separator: '\t', headers: ["1", "2", '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'] })).on('data', (data) => convertToRuTable.push(data)).on('end', () => { console.log(convertToRuTable); })
+const convertToRuTable_ = R.pipe(
+  R.sortBy((x) => x.from.length),
+  R.reverse
+)(convertToRuTable)
 
-convertToRuTable = convertToRuTable.map(x => ({ from: x['1'], to: x['10'] })).sort((a, b) => a.from.length - b.from.length).reverse()
-
-
-res3 = res2.map(x => {
-  let p = x['p']
-
-  for (const { from, to } of convertToRuTable) {
-    p = p.replace(new RegExp(from, 'i'), to)
+async function mymapper(x) {
+  const convertPinyinMarkedToNumbered = async (text) => {
+    let words = require('pinyin-split').default(text, true)
+    words = require('pinyin-utils').markToNumber(words, false)
+    return words.join('')
   }
+
+  const convertPinyinNumberedToRu = (text) => {
+    let ru = text
+    for (const { from, to } of convertToRuTable_) {
+      ru = ru.replace(new RegExp(from, 'i'), to)
+    }
+    return ru
+  }
+
+  const sentence = x['sentence']
+
+  if (sentence == 'Iliketoeatcucumbersandcheese.') { return null }
+
+  // console.log({ sentence, x })
+
+  const pinyin_marked = await (x['pinyin_marked'] ? Promise.resolve(x['pinyin_marked']) : require('hanzi-to-pinyin')(sentence))
+  console.log({ pinyin_marked, sentence, x })
+  const pinyin_numbered = await convertPinyinMarkedToNumbered(pinyin_marked)
+  // console.log({ pinyin_numbered })
+  const pinyin_ru = convertPinyinNumberedToRu(pinyin_numbered)
+  // console.log({ pinyin_ru })
 
   return {
-    sentence: x['sentence'],
-    before: x['p'],
-    after: p
+    sentence,
+    pinyin_marked,
+    pinyin_numbered,
+    pinyin_ru,
   }
-})
+}
+
+await Promise.all(input.map(mymapper))
+
+JSON.stringify(input.filter(x => x['pinyin_marked'] == '' || x['sentence'] != 'Iliketoeatcucumbersandcheese.').map(x => x['sentence']))
 
 require('csv-writer').createObjectCsvWriter({ path: '/home/srghma/Downloads/Chinese Grammar Wiki2.txt', header:  ["sentence", "before", "after"].map(x => ({ id: x, title: x })) }).writeRecords(res3).then(() => { console.log('Done') })
