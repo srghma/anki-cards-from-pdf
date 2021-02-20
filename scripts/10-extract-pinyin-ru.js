1e95537663f7d4e834d014a700b640bd46ec42d3
Serhii Khoma <srghma@gmail.com>
2021-02-20 20:46:20 +0200

    feat: update

--- scripts/10-extract-pinyin-ru.js
+++ scripts/10-extract-pinyin-ru.js
@@ -5,3 +5,23 @@ const R = require('ramda')
 
 input = await readStreamArray(fs.createReadStream('/home/srghma/Downloads/Chinese_ Sherlock.txt').pipe(csv({ separator: "\t", headers: [ "id", "hanzi", "ruby" ] })))
 
+function exRu(text) {
+  text.match(/"singlebk".+?"singlebk"/g).map((s) => {
+    return s.match(/pinyin-ru">(.+?)<\/span/)
+  })
+}
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const dom = new JSDOM(text);
+
+function removeAllNodes(elements) {
+  elements.forEach(e => {
+    e.parentNode.removeChild(e);
+  })
+}
+
+dom.window.document.body.innerHTML
+removeAllNodes(dom.window.document.querySelectorAll('.pinyin-marked'))
+
+exRu(input[0].ruby)
