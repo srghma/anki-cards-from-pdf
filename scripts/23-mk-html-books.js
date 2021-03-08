1e02a16611ab0f0dade2d6b38ca3729b6c0fcdcd
Serhii Khoma <srghma@gmail.com>
2021-03-08 22:07:59 +0200

    feat: update

--- scripts/23-mk-html-books.js
+++ scripts/23-mk-html-books.js
@@ -7,7 +7,7 @@ folders = [
   { pdf: "/home/srghma/Desktop/languages/chinese/Sherlock_Holmes.pdf", outputdir: "/home/srghma/Desktop/languages/chinese/Sherlock" }
 ]
 
-async function dowork({ pdf, outputdir }) {
+async function createHtml({ pdf, outputdir }) {
   await exec(`rm -rfd ${outputdir}`)
   await exec(`mkdir -p ${outputdir}`)
   await exec(`pdftohtml ${pdf} ${outputdir}/myhtml`)
@@ -26,16 +26,43 @@ async function dowork({ pdf, outputdir }) {
   text = text.replace(/<b>(\d+)<\/b><br\/>\n([^<]+)<br\/>/g, '<b><i>$1</i> $2</b><br/>')
 
   text = text.replace(/﻿/g, '')
-  text = text.replace(/([^(\.|。|>)])(<\/a>)?<br\/>\n/g, '$1$2')
+  text = text.replace(/([^(>|！|。|…|\.)])(<\/a>)?<br\/>\n/g, '$1$2')
+  // text = text.replace(/<br\/>/g, '<br/>\n')
 
-  text = text.replace(/(<style type="text\/css">\n)/g, `$1img {
+  const textWithoutHtml = text.replace(/(<([^>]+)>)/gi, "")
+
+  text = text.replace(/(<style type="text\/css">)\n/g, `$1
+img {
   max-width: 90%;
+  max-height: 90%;
   display: block;
   margin-left: auto;
   margin-right: auto;
-}`)
+}
+a {
+  text-decoration: none;
+}
+`)
 
+  await fs.writeFile(`${outputdir}/myhtmls-w.txt`, textWithoutHtml)
   await fs.writeFile(`${outputdir}/myhtmls.html`, text)
+  await fs.writeFile(`${outputdir}/myhtmls.txt`, text)
 }
 
-Promise.all(folders.map(dowork))
+async function processPurplecultureHtml({ outputdir }) {
+  let text = await fs.readFile(`${outputdir}/myhtmls-processed.html`)
+
+  text = text.toString()
+
+  text = text.replace(/< /g, '<')
+  text = text.replace(/ >/g, '>')
+  text = text.replace(/ \/>/g, '/>')
+  text = text.replace(/<\/ /g, '</')
+  text = text.replace(/href = " myhtmls . html # (\d+) "/g, 'href="myhtmls.html#$1"')
+  text = text.replace(/href="myhtmls.html#/g, 'href="myhtmls-processed.html#')
+
+  await fs.writeFile(`${outputdir}/myhtmls-processed.html`, text)
+}
+
+Promise.all(folders.map(processPurplecultureHtml))
+// Promise.all(folders.map(createHtml))
