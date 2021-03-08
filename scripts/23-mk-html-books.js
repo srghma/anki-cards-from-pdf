6911b49d825dc000ce0a126ed2764fae239b7617
Serhii Khoma <srghma@gmail.com>
2021-03-08 20:50:30 +0200

    feat: update

---
+++ scripts/23-mk-html-books.js
@@ -0,0 +1,41 @@
+const exec = require('await-exec')
+const fs = require('fs').promises
+const fixRadicalToKanji = require('./lib/fixRadicalToKanji').fixRadicalToKanji
+
+folders = [
+  { pdf: "/home/srghma/Desktop/languages/chinese/In_Search_of_Hua_Ma.pdf", outputdir: "/home/srghma/Desktop/languages/chinese/HuaMa" },
+  { pdf: "/home/srghma/Desktop/languages/chinese/Sherlock_Holmes.pdf", outputdir: "/home/srghma/Desktop/languages/chinese/Sherlock" }
+]
+
+async function dowork({ pdf, outputdir }) {
+  await exec(`rm -rfd ${outputdir}`)
+  await exec(`mkdir -p ${outputdir}`)
+  await exec(`pdftohtml ${pdf} ${outputdir}/myhtml`)
+
+  let text = await fs.readFile(`${outputdir}/myhtmls.html`)
+
+  text = text.toString()
+  text = fixRadicalToKanji(text)
+
+  text = text.replace(/&#160;/g, ' ')
+  text = text.replace(/\[\d+\]/g, '')
+  text = text.replace(/(.)<br\/>(.)/g, '$1 $2')
+  text = text.replace(/&#34;/g, '"')
+  text = text.replace(/<b>\s*<\/b>/g, '')
+  text = text.replace(/<a.*?>\s*<\/a>/g, '')
+  text = text.replace(/<b>(\d+)<\/b><br\/>\n([^<]+)<br\/>/g, '<b><i>$1</i> $2</b><br/>')
+
+  text = text.replace(/﻿/g, '')
+  text = text.replace(/([^(\.|。|>)])(<\/a>)?<br\/>\n/g, '$1$2')
+
+  text = text.replace(/(<style type="text\/css">\n)/g, `$1img {
+  max-width: 90%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}`)
+
+  await fs.writeFile(`${outputdir}/myhtmls.html`, text)
+}
+
+Promise.all(folders.map(dowork))
