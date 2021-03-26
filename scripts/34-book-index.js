985b8446cd585a19818c01e1cac6950443499eea
Serhii Khoma <srghma@gmail.com>
2021-03-26 17:41:50 +0200

    feat: update

---
+++ scripts/34-book-index.js
@@ -0,0 +1,25 @@
+const readStreamArray = require('./scripts/lib/readStreamArray').readStreamArray
+const checkDuplicateKeys = require('./scripts/lib/checkDuplicateKeys').checkDuplicateKeys
+const isHanzi = require('./scripts/lib/isHanzi').isHanzi
+const mkQueue = require('./scripts/lib/mkQueue').mkQueue
+const mapWithForEachToArray = require('./scripts/lib/mapWithForEachToArray').mapWithForEachToArray
+const csv = require('csv-parser')
+const fs = require('fs')
+const R = require('ramda')
+const RA = require('ramda-adjunct')
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const dom = new JSDOM(``);
+const {Translate} = require('@google-cloud/translate').v2;
+const translate = new Translate({projectId: "annular-form-299211"});
+const svg2img = require('svg2img')
+const btoa = require('btoa')
+const mkdirp = require('mkdirp')
+
+content = R.uniq(fs.readFileSync('/home/srghma/Desktop/languages/chinese/Sherlock/myhtmls-w.txt').toString().split('').filter(isHanzi)).map((x, i) => ({ kanji: x, i: i + 1 }))
+
+;(function(input){
+  const header = Object.keys(input[0]).map(x => ({ id: x, title: x }))
+  const s = require('csv-writer').createObjectCsvStringifier({ header, fieldDelimeter: ";" }).stringifyRecords(input)
+  fs.writeFileSync('/home/srghma/Downloads/Chinese Grammar Wiki2.txt', s)
+})(content);
