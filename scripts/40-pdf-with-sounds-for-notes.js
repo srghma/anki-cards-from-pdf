eaaab1eb103cf7cd10e364063e32e133e54ca01c
Serhii Khoma <srghma@gmail.com>
2021-04-17 08:48:20 +0300

    feat: update

--- scripts/40-pdf-with-sounds-for-notes.js
+++ scripts/40-pdf-with-sounds-for-notes.js
@@ -1,14 +1,10 @@
 // ./node_modules/.bin/babel-node --config-file ./babel.config.js scripts/40-pdf-with-sounds-for-notes.js
-import * as R from 'ramda'
-import React from 'react'
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer'
-import ReactPDF from '@react-pdf/renderer'
+const R = require('ramda')
+// import React from 'react'
+// import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer'
+// import ReactPDF from '@react-pdf/renderer'
 
-const styles = StyleSheet.create({
-  section: { color: 'white', textAlign: 'center', margin: 30 }
-});
-
-t = `a	ai	ao	an	ang	e	ei	en	eng	er	o	ou		yi		ya	yao	ye	you	yan	yang	yin	ying	yong	wu	wa	wai	wei	wo	wan	wang	wen	weng	yu	yue	yuan	yun
+let t = `a	ai	ao	an	ang	e	ei	en	eng	er	o	ou		yi		ya	yao	ye	you	yan	yang	yin	ying	yong	wu	wa	wai	wei	wo	wan	wang	wen	weng	yu	yue	yuan	yun
 ba	bai	bao	ban	bang		bei	ben	beng		bo			bi			biao	bie		bian		bin	bing		bu
 pa	pai	pao	pan	pang		pei	pen	peng		po	pou		pi			piao	pie		pian		pin	ping		pu
 ma	mai	mao	man	mang	me	mei	men	meng		mo	mou		mi			miao	mie	miu	mian		min	ming		mu
@@ -31,22 +27,53 @@ ga	gai	gao	gan	gang	ge	gei	gen	geng			gou	gong												gu	gua	guai	gui	guo	gu
 ka	kai	kao	kan	kang	ke	kei	ken	keng			kou	kong												ku	kua	kuai	kui	kuo	kuan	kuang	kun
 ha	hai	hao	han	hang	he	hei	hen	heng			hou	hong												hu	hua	huai	hui	huo	huan	huang	hun`
 
-t = t.split('\n').map(R.trim()).filter(R.identity).map(R.split('\t')).flat().flat().filter(R.identity)
+t = t.split('\n').map(R.trim()).filter(R.identity).map(line => ({ s: line[0], lines: R.split('\t', line).flat().flat().filter(R.identity) }))
 
-const doc = (
-  <Document>
-  (t.forEach(sound => {
-    return (
-      <Page size="A4">
-        ( [""]
-        )
-        <View style={{ color: 'white', textAlign: 'center', margin: 30 }}>
-          <Text>Section #1</Text>
-        </View>
-      </Page>
-    )
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+
+// Create a document
+const doc = new PDFDocument();
+const { outline } = doc;
+
+// Pipe its output somewhere, like to a file or HTTP response
+// See below for browser usage
+doc.pipe(fs.createWriteStream(`${process.cwd()}/chinese-table-of-sounds.pdf`));
+
+t.forEach(({ s, lines }, sIndex) => {
+  const top = outline.addItem(s);
+
+  lines.forEach((line, lineIndex) => {
+    doc
+      .font('fonts/PalatinoBold.ttf')
+      .fontSize(13)
+      .text(line, 10, 10);
+
+    const types = "ˉ ´ ˉ `".split(' ')
+
+    types.forEach((type, lineIndex) => {
+      doc
+        .font('fonts/PalatinoBold.ttf')
+        .fontSize(20)
+        .text(type, 70 + 150 * lineIndex, 30);
+    })
+
+    // const isFirstPage = lineIndex == 0
+    const isSLastPage = sIndex == t.length - 1
+    const isLastPage = lineIndex == lines.length - 1
+
+    top.addItem(line);
+
+    if (!(isSLastPage && isLastPage)) {
+      doc.addPage({
+        margins: {
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0
+        }
+      })
+    }
   })
-  </Document>
-);
-
-ReactPDF.render(doc, `${process.cwd()}/chinese-table-of-sounds.pdf`);
+})
+doc.end();
