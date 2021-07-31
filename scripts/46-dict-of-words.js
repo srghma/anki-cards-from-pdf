17e9629c9ccf5ca27a8846a3c7279a0dbd8e8d3b
Serhii Khoma <srghma@gmail.com>
2021-07-31 15:37:03 +0300

    update

--- scripts/46-dict-of-words.js
+++ scripts/46-dict-of-words.js
@@ -44,7 +44,7 @@ function splitDictOnEntries(dict) {
 }
 withoutComments = x => x.filter(x => !x.startsWith('#'))
 comments = x => x.filter(x => x.startsWith('#'))
-dabkrs_210426__orig = fs.readFileSync('/home/srghma/Downloads/dabkrs_210426').toString().split('\n')
+dabkrs_210426__orig = fs.readFileSync('/home/srghma/Downloads/dabkrs_210627').toString().split('\n')
 dabkrs_210426_split = splitDictOnEntries(withoutComments(dabkrs_210426__orig.map(R.trim))).filter(R.identity)
 toRecord = xs => {
   const buffer = {}
@@ -126,19 +126,15 @@ await mkQueue(10).addAll(promises)
 // output.filter(x => x.translation.length > 1)
 
 output_ = R.sortBy(x => x.gPlecoPinyin.toLowerCase(), output)
-
 output_ = output_.map(x => {
   let translation = x.translation
   translation = translation.filter(y => y.simplified === x.Simplified)
   translation = R.sortBy(x => x.english.length, translation)
-
   return {
     ...x,
     translation: translation[translation.length - 1]
   }
-})
-
-output_ = output_.map(x => ({ ...x, ...(x.translation), translation: null }))
+}).map(x => ({ ...x, ...(x.translation), translation: null }))
 
 replaces = [
   // { search: '[m]',    replace: '<div>' },
@@ -196,9 +192,142 @@ function fixTransl(t) {
 colorize = ch => `<span onclick="window.showKanjiIframe('${ch}')">${ch}</span>`
 colorizes = s => s.split('').map(ch => isHanzi(ch) ? colorize(ch) : ch).join('')
 
+link = (ch, t) => `<a target="_blank" href="plecoapi://x-callback-url/s?q=${encodeURIComponent(ch)}">${t}</a>`
+
+groupByAndToArray = (byFn, key, val, input) => R.toPairs(R.groupBy(byFn, input)).map(x => ({ [key]: x[0], [val]: x[1] }))
+
+tOrig = fs.readFileSync('./table-with-tabs.txt').toString()
+tOrigLines = tOrig.split('\n')
+splitLine = line => R.split('\t', line).flat().flat()
+sections_ = tOrigLines.filter(R.identity).map(line => ({ sectionLetter: line.trim()[0], pinyins: splitLine(line) }))
+toSectionId = x => x.replace(/ü/g, 'v')
+
+html = output_.filter(x => x.hsk > 0 || (x.ru && (x.english || '').length > 0 && x.gRank_Word < 10000))
+html = R.sortBy(x => x.pinyinNumbers.toLowerCase(), html)
+html = html.map(x => {
+  ms = R.zip(x.pinyinMarks.split(' '), x.toneMarks).map(([x, t]) => String.raw`<span class="tone${t}">${x}</span>`).join(' ')
+  ms = [
+    ms,
+    link(x.Simplified, 'simp'),
+    x.Traditional == x.Simplified ? null : link(x.Traditional, 'trad'),
+  ].filter(R.identity).join('<br>')
+
+  const hsk = x.hsk > 0 ? x.hsk : ''
+  // const sim = colorizes(x.Simplified)
+  const sim = x.Simplified
+  const trad = x.Traditional == x.Simplified ? '' : x.Traditional
+  const marks = ms
+  const en = x.english.join(`<br>`)
+  const ru = x.ru.replace(/\[m\d\]/g, '').split('[/m]').map(R.trim).filter(R.identity).filter(x => !x.startsWith('[ex]') && !x.startsWith('[*]')).map(fixTransl).join('<br>')
+
+  const front = `
+
+<div class="hsk">${hsk}</div>
+<div class="my-pinyin-hanzi">
+<div class="sim">${sim}</div>
+<div class="trad">${trad}</div>
+<div class="marks">${marks}</div>
+</div>
+<div class="en">${en}</div>
+<div class="ru">${ru}</div>
+`
+  const marked = x.pinyinMarks.split(' ')[0].toLowerCase()
+  const sectionLetter = x.pinyinNumbers.split(' ')[0].toLowerCase().replace(/\d/, '')
+  return { front, marked, sectionLetter, hsk: x.hsk > 0 ? x.hsk : null, gRank_Word: x.gRank_Word }
+})
+
+// trace = x => { console.log(x); return x }
+// html = groupByAndToArray(R.prop('sectionLetter'), "sectionLetter", "pinyins", html).map(x => ({ sectionLetter: x.sectionLetter, pinyins: groupByAndToArray(R.prop('marked'), "marked", "front", x.pinyins).map(x => ({ marked: x.marked, front: R.sortBy(x => x.hsk || 9999, x.front).map(x => x.front).join('<hr>') })) }))
+//
+// // html[0].pinyins
+//
+// html_ = `
+// <!DOCTYPE HTML>
+// <html>
+//  <head>
+//   <meta charset="utf-8">
+//   <title>Hua ma</title>
+//   <link rel="stylesheet" href="main.css">
+//   <style>
+// .hsk-1, .hsk-2, .hsk-3, .hsk-4, .hsk-5, .hsk-6 { background-color: red; }
+// .mytable tr > *:nth-child(1) { width: 1%; white-space: nowrap; }
+// .mytable tr > *:nth-child(2) { width: 1%; white-space: nowrap; }
+// .mytable tr > *:nth-child(3) { width: 1%; white-space: nowrap; }
+// .mytable tr > *:nth-child(4) { width: 1%; white-space: nowrap; }
+// .tone1 { color: #a3a3ff; }
+// .tone2 { color: lightgreen; }
+// .tone3 { color: #ff00ff; }
+// .tone4 { color: #ff7b7b; }
+//
+// .pinyin__containers { display: flex; }
+// .pinyin__root_container { display: none; }
+// .pinyin__root_container--show { display: block; }
+// .pinyin__container { flex-grow: 1; flex-basis: 0; }
+//   </style>
+//   <script src="dict-myscripts.js"></script>
+//  </head>
+//  <body id="ruby" class="nightMode my-pinyin-hanzi_container--front trainchinese-container--hide-hanzi purpleculture_pinyin--front">
+//   <div class="table-container">
+//     <table border="1">
+//     ${sections_.map(x => String.raw`<tr>` + x.pinyins.map(x => x ? String.raw`<a onclick="window.showRootContainer('${x}')" href="#${x}">${x}</a>` : '').map(x => String.raw`<td>${x}</td>`).join('') + String.raw`</tr>`).join('\n')}
+//     </table>
+//   </div>
+// ${
+// html.map(({ sectionLetter, pinyins }) => {
+//   return String.raw`
+// <div id="pinyin__root_container__${sectionLetter}" class="pinyin__root_container">
+// <h1><a id="${sectionLetter}"></a>${sectionLetter}</h1>
+// <div class="pinyin__containers">
+// ${
+// pinyins.map(pinyin => {
+// return String.raw`
+// <div class="pinyin__container">
+// <div class="pinyin__header">${pinyin.marked}</div>
+// <div class="pinyin__item">${pinyin.front}</div>
+// </div>`
+// }).join('\n')
+// }
+// </div>
+// </div>
+// `
+// }).join('\n')
+// }
+// </div>
+// </div>
+// <script src="myscripts.js"></script>
+//  </body>
+// </html>`
+//
+// fs.writeFileSync('/home/srghma/projects/anki-cards-from-pdf/html/dict-show-6000-transl.html', html_)
+
+////////////////////
+
+colorize = ch => `<span onclick="window.showKanjiIframe('${ch}')">${ch}</span>`
+colorizes = s => s.split('').map(ch => isHanzi(ch) ? colorize(ch) : ch).join('')
+
 link = (ch, t) => `<a target="_blank" href="plecoapi://x-callback-url/s?q=${ch}">${t}</a>`
 
-html = output_.filter(x => x.hsk > 0 || (x.ru && (x.english || '').length > 0 && x.gRank_Word < 7000)).map(x => {
+tOrig = fs.readFileSync('./table-with-tabs.txt').toString()
+tOrigLines = tOrig.split('\n')
+tOrigLines = tOrigLines.map(x => x.split('\t')).flat().flat().filter(x => x)
+tOrigLines__ = tOrigLines.map(x => x.replace(/(l|n)ü/g, '$1u:').replace(/ü/g, 'u'))
+// html = output_.filter(x => x.hsk > 0 || (x.ru && (x.english || '').length > 0 && x.gRank_Word < 15000))
+html = output_.filter(x => x.hsk > 0)
+html = R.sortBy(x => {
+  const xx = x.pinyinNumbers.toLowerCase().split(' ').map(pinyinAndNumber => {
+    let pinyin = pinyinAndNumber.replace(/\d+/g, '')
+    const number = pinyinAndNumber.replace(/\D+/g, '')
+
+    if (pinyin === 'r') { pinyin = 'er' }
+
+    const pinyinIndex = tOrigLines__.indexOf(pinyin)
+    if (pinyinIndex === -1) { throw new Error(pinyin) }
+    return [pinyinIndex, number]
+  })
+  return xx
+}, html)
+
+html = html.map(x => {
   ms = R.zip(x.pinyinMarks.split(' '), x.toneMarks).map(([x, t]) => String.raw`<span class="tone${t}">${x}</span>`).join(' ')
 
   ms = [
@@ -214,7 +343,7 @@ html = output_.filter(x => x.hsk > 0 || (x.ru && (x.english || '').length > 0 &&
     ["trad", x.Traditional == x.Simplified ? '' : colorizes(x.Traditional)],
     ["marks", ms],
     ["en", colorizes(x.english.join(`<br>`))],
-    ["ru", colorizes(x.ru.replace(/\[m\d\]/g, '').split('[/m]').map(R.trim).filter(R.identity).filter(x => !x.startsWith('[ex]') || !x.startsWith('[*]')).map(fixTransl).join('<br>'))],
+    ["ru", colorizes(x.ru.replace(/\[m\d\]/g, '').split('[/m]').map(R.trim).filter(R.identity).filter(x => !x.startsWith('[ex]') && !x.startsWith('[*]')).map(fixTransl).join('<br>').replace(/[\u4e00-\u9fff]|[\u3400-\u4dbf]|[\u{20000}-\u{2a6df}]|[\u{2a700}-\u{2b73f}]|[\u{2b740}-\u{2b81f}]|[\u{2b820}-\u{2ceaf}]|[\uf900-\ufaff]|[\u3300-\u33ff]|[\ufe30-\ufe4f]|[\uf900-\ufaff]|[\u{2f800}-\u{2fa1f}]|[\u3040-\u309f]|[\u30a0-\u30ff]|[\u4e00-\u9faf]|[\u3400-\u4dbf]/gu, ''))],
   ].map(([kl, x]) => String.raw`<td class="${kl}">${x}</td>`)
 })
 
@@ -225,20 +354,37 @@ html_ = `
   <meta charset="utf-8">
   <title>Hua ma</title>
   <link rel="stylesheet" href="main.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap" rel="stylesheet">
   <style>
   .hsk-1, .hsk-2, .hsk-3, .hsk-4, .hsk-5, .hsk-6 { background-color: red; }
   .mytable tr > *:nth-child(1) { width: 1%; white-space: nowrap; }
-  .mytable tr > *:nth-child(2) { width: 1%; white-space: nowrap; }
-  .mytable tr > *:nth-child(3) { width: 1%; white-space: nowrap; }
+  .mytable tr > *:nth-child(2) { width: 1%; white-space: nowrap; font-size: 27px; font-family: 'Noto Sans SC', sans-serif; }
+  .mytable tr > *:nth-child(3) { width: 1%; white-space: nowrap; font-size: 27px; font-family: 'Noto Sans SC', sans-serif; }
   .mytable tr > *:nth-child(4) { width: 1%; white-space: nowrap; }
-  .tone1 { color: #a3a3ff; }
-  .tone2 { color: lightgreen; }
+
+  table { page-break-inside:auto }
+  tr    { page-break-inside:avoid; page-break-after:auto }
+  thead { display:table-header-group }
+  tfoot { display:table-footer-group }
+
+  .marks a { color: black; }
+  .tone1 { color: #0000ff; }
+  .tone2 { color: green; }
   .tone3 { color: #ff00ff; }
-  .tone4 { color: #ff7b7b; }
+  .tone4 { color: #ff0000; }
+  .reference { display: none; }
+
+  .nightMode .marks a { color: white; }
+  .nightMode .tone1 { color: #a3a3ff; }
+  .nightMode .tone2 { color: lightgreen; }
+  .nightMode .tone3 { color: #ff00ff; }
+  .nightMode .tone4 { color: #ff7b7b; }
   </style>
   <script src="dict-myscripts.js"></script>
  </head>
- <body class="nightMode">
+ <body class="nonightMode">
   <table border="1" class="mytable">
    ${html.map(x => String.raw`<tr>${x.join('')}</tr>`).join('\n')}
   </table>
@@ -248,4 +394,5 @@ html_ = `
 </html>
 `
 
-fs.writeFileSync('/home/srghma/projects/anki-cards-from-pdf/html/dict.html', html_)
+fs.writeFileSync('/home/srghma/projects/anki-cards-from-pdf/html/dict-hsk.html', html_)
+
