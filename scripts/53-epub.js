0d23ac0921d60ab48e97d12eaf56200dee86ba45
Serhii Khoma <srghma@gmail.com>
2021-10-30 13:02:20 +0300

    feat: add working tests

--- scripts/53-epub.js
+++ scripts/53-epub.js
@@ -88,15 +88,106 @@ image = await getImage(epub, 'x001.jpg')
 
 output = '/home/srghma/projects/anki-cards-from-pdf/html/elon-musk'
 
-await (require('mkdirp'))(output)
+// R.toPairs(epub.manifest).forEach(async ([id, data]) => {
+//   if (data['media-type'] !== 'image/jpeg') { return }
+//   const image = await getImage(epub, id)
+//   const outputPath = `${output}/${data.href.replace('OEBPS/', '')}`
+//   await (require('mkdirp'))(require('path').dirname(outputPath))
+//   // console.log(path)
+//   await require('fs/promises').writeFile(outputPath, image.data)
+// })
 
-R.toPairs(epub.manifest).forEach(([id, data]) => {
-  if (data['media-type'] !== 'image/jpeg') { return }
+// https://www.sitepoint.com/removing-useless-nodes-from-the-dom/
+function clean(node)
+{
+  for(var n = 0; n < node.childNodes.length; n ++) {
+    var child = node.childNodes[n];
+    if (
+      child.nodeType === 8
+      ||
+      (child.nodeType === 3 && !/\S/.test(child.nodeValue))
+    )
+    {
+      node.removeChild(child);
+      n--;
+    }
+    else if(child.nodeType === 1) {
+      clean(child);
+    }
+  }
+}
+
+// data = epub.flow[2]
+html = epub.flow.map(async (data) => {
+  if (data['media-type'] !== 'application/xhtml+xml') { return }
+  let epubFile = await getFile(epub, data.id)
+  epubFile = epubFile.toString()
+
+  const outputPath = `${output}/${data.href.replace('OEBPS/', '')}`.replace('.xhtml', '.html')
+  await (require('mkdirp'))(require('path').dirname(outputPath))
+
+  mydom = new JSDOM(epubFile)
+
+  clean(mydom.window.document.body)
+
+  // console.log(outputPath)
+  // console.log(epubFile)
+
+  const html = mydom.window.document.body.innerHTML
+
+  return { ...data, html }
 })
 
-// file = fs.readFileSync(`/home/srghma/projects/anki-cards-from-pdf/html/elon-musk.html`).toString()
+html = await Promise.all(html)
 
-// colorize = ch => `<a target="_blank" href="plecoapi://x-callback-url/s?q=${ch}">${ch}</a>`
+css = R.values(epub.manifest).map(async (data) => {
+  if (data['media-type'] !== 'text/css') { return }
+  let epubFile = await getFile(epub, data.id)
+  css = epubFile.toString()
+  return { ...data, css }
+})
+css = await Promise.all(css)
+css = css.filter(Boolean)
+css.forEach(async data => {
+  const outputPath = `${output}/${data.href.replace('OEBPS/', '')}`
+  await (require('mkdirp'))(require('path').dirname(outputPath))
+  console.log(outputPath, data.css)
+  await require('fs/promises').writeFile(outputPath, data.css)
+})
+
+toc = await getFile(epub, 'ncx')
+toc = toc.toString()
+dom.window.document.body.innerHTML = toc
+dom.window.document.body.querySelector('navmap').outerHTML
+
+html_ = `
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>${epub.metadata.title}</title>
+  ${css.map(x => `<link rel="stylesheet" href="${x.href.replace('OEBPS/', '')}">`).join('\n')}
+  <style>
+    chapter { position: relative; }
+    div.juzhong2 { position: initial; }
+  </style>
+ </head>
+ <body class="nightMode">
+  ${html.map(x => '<chapter>' + x.html + '</chapter>').join('\n').replace(/src="\.\.\/Images/g, 'src="Images').replace(/\.\.\/Text\/chapter\d\d\.xhtml#/g, '#')}
+</div>
+</div>
+ </body>
+</html>
+`
+
+fs.writeFileSync(`/home/srghma/projects/anki-cards-from-pdf/html/elon-musk/index.html`, html_)
+
+// file = fs.readFileSync(`/home/srghma/projects/anki-cards-from-pdf/html/elon-musk.html`).toString()
+// mydom = new JSDOM(file)
+// clean(mydom.window.document.body)
+
+// // colorize = ch => `<a target="_blank" href="plecoapi://x-callback-url/s?q=${ch}">${ch}</a>`
+// colorize = ch => `<a target="_blank" href="/h.html#${ch}">${ch}</a>`
 // colorizes = s => [...s].map(ch => isHanzi(ch) ? colorize(ch) : ch).join('')
 
-// fs.writeFileSync(`/home/srghma/projects/anki-cards-from-pdf/html/elon-musk.html`, colorizes(file))
+// fs.writeFileSync(`/home/srghma/projects/anki-cards-from-pdf/html/elon-musk.html`, colorizes(mydom.serialize()))
