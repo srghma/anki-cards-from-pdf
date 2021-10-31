af7fae03135bd28965d3e2f926fdfb4c7a5237d9
Serhii Khoma <srghma@gmail.com>
2021-10-31 14:59:17 +0200

    feat: pinyin

--- scripts/52-ru-pinyin.js
+++ scripts/52-ru-pinyin.js
@@ -192,7 +192,7 @@ x = x.map(({ file, x }) => {
   return x
 })
 // x = x.filter(x => x.hanzi.length !== 0)
-await require('fs/promises').writeFile('/home/srghma/projects/anki-cards-from-pdf/html/ru-pinyin', x.filter(Boolean).join('\n\n----\n\n') + '\n')
+await require('fs/promises').writeFile('/home/srghma/projects/anki-cards-from-pdf/html/ru-pinyin', x.filter(Boolean).sort().join('\n\n----\n\n') + '\n')
 
 /////////////////
 
